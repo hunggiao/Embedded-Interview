981f694aee70bbef6a33329b569ec63b15092b3e
hunggiao <133474779+hunggiao@users.noreply.github.com>
2023-05-26 21:00:39 +0700

    Update static.c

--- Pointer/Static/static.c
+++ Pointer/Static/static.c
@@ -2,7 +2,7 @@
 
 //lay 1 bien tu 1 thu vien khac 
 //khong the lay bien (ham) static tu thu vien khac duoc
-extern int count;
+//extern int count;
 extern void dem();
 
 int main(int argc, char const *argv[])
