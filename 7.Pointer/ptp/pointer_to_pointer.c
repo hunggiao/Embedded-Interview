532721225d1e60a3381f84ebfea14b9222cdbdc9
giao <hunggiaopro@gmail.com>
2023-06-02 16:10:16 +0700

    Giao

--- 7.Pointer/ptp/pointer_to_pointer.c
+++ 7.Pointer/ptp/pointer_to_pointer.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
     char array1[] = "Hello"; //stack
     char *array2[] = { "Hi", "Hello", "world"}; //pointer to pointer
     printf("test: %s\n", *array2); //Hi
-    printf("test: %c\n", *(*array2+1));//i
+    printf("test: %c\n", *(*(array2+1)+1));//e
 
     /*int a = 27;
     int *ptr = &a;
--- BaitapC/bai3.c
+++ BaitapC/bai3.c
@@ -1,11 +1,22 @@
+// dao nguoc chuoi (dao tung tu)
+
 #include<stdio.h>
 #include<string.h>
+
+int length (char array[]){
+    int i = 0;
+    while (array[i]!='\0'){
+        i++;
+    }
+    return i;
+}
+
 
 int main(){
     char array[] = {"mot hai ba, bon nam"};
     printf("Chuoi ban dau: %s\n",array);
     int len;
-    len=strlen(array);
+    len=length(array);
     printf("Chuoi sau khi dao: ");
     for(int i=len;i>=0;--i){
         if(array[i]==' '){
